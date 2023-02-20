4da421aa7d9d7b9952ecd63bb586c168b2d6e232
AyanNaseer <ayannaseer70065988@gmail.com>
2023-02-20 18:59:41 +0500

    Hello

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(myExpress.json());
 require("./db/db")
 
 
-// app.use(myExpress.static('./build'));
 app.use(userRoute)
 app.use(companyRoute)
 app.use(searchRoute)
 app.use(reviewRoute)
+app.use(myExpress.static(path.resolve(__dirname,'build')));
+
+app.get("*",(req,resp)=>{
+        resp.sendFile(path.resolve(__dirname+'/build/index.html'))
+})
 
 app.listen(process.env.PORT||2700,()=>{console.log("Hello, Sever is running")})
