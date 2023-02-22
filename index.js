0a59572e36b5f972520c2ba67dcd2ab38897d496
AyanNaseer <ayannaseer70065988@gmail.com>
2023-02-22 14:23:51 +0500

    Make some Changes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const userRoute = require('./routes/users')
 const companyRoute = require('./routes/company')
 const searchRoute = require('./routes/search')
 const reviewRoute = require('./routes/review')
+const adminRoute = require('./routes/admin')
 app.use(myExpress.json());
 const path = require('path')
+
 
 require("./db/db")
 
@@ -13,8 +15,8 @@ require("./db/db")
 app.use(userRoute)
 app.use(companyRoute)
 app.use(searchRoute)
+app.use(adminRoute)
 app.use(reviewRoute)
-
 
 app.use(myExpress.static(path.resolve(__dirname,'build')));
 
