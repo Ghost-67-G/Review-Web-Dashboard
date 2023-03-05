896fcd2e3df53364f3d76ada2d481df1d5e160ca
Ghost-67-G <ayannaseer70065988@gmail.com>
2023-03-05 13:21:31 +0500

    Ayan

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 let mongoose = require("mongoose");
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://custmerg476:Y2xmDrPYuHOtdPl1@cluster.gyxsstr.mongodb.net/ReviewSite?w=majority', (err , connection)=>{
+
+mongoose.connect('mongodb+srv://mrjay:yZxeO3CC4A12LSHo@propfirmrating.ozjnr5r.mongodb.net/ReviewSite?w=majority', (err , connection)=>{
 
     console.log( connection || err);
 
